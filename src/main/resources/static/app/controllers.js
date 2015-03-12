1c18966bfa08c70d23e123ade4079542e1b52d8e
Geraldo Castro <geraldofcneto@gmail.com>
2015-03-12 15:57:16 -0300

    Creating bower structure

---
+++ src/main/resources/static/app/controllers.js
@@ -0,0 +1,52 @@
+(function(angular) {
+	var AppController = function($scope, $http) {
+		$scope.bogus = 'Bogus';
+		$scope.nominees = [];
+		var session = '';
+		
+		var loadNominees = function(data){
+			console.log(JSON.stringify(data));
+			session = data.session;
+			$scope.nominees = data.nominees;
+		};
+		
+		var getNominees = function (){
+			$http({
+				url : 'api/vote-no-livro',
+				method : 'GET',
+				params : {
+					session_id : session.id 
+				}
+			}).success(loadNominees).error(function (e){
+				console.log('Error: '+e);
+			}); 
+		};
+		
+		$scope.vote = function (winner){
+			$scope.nominees.splice($scope.nominees.indexOf(winner), 1);
+
+			var request = {
+					url : 'api/vote-no-livro',
+					method : 'POST',
+					data : {
+						session: session,
+						winner: winner,
+						loser: $scope.nominees[0]
+					}
+				};
+			
+			console.log(JSON.stringify(request));
+			
+			$http(request).success(loadNominees).error(function (e){
+				console.log('Error: '+JSON.stringify(e));
+			}); 
+		};
+		
+		
+		getNominees();
+	};
+
+	AppController.$inject = [ '$scope', '$http' ];
+	angular.module("myApp.controllers").controller("AppController",
+			AppController);
+}(angular));
